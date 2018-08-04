4b4eb7d61c7884002d6e7d1e52beb1a33a8b0528
Arkadiusz Miśkiewicz <arekm@maven.pl>
2018-08-04 22:08:58 +0200

    - add branch script

---
+++ branch.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+url=https://git.sipsolutions.net/dovecot-antispam.git/
+package=dovecot-antispam
+tag=v2.0
+branch=master
+out=$package-git.patch
+repo=$package.git
+
+# use filterdiff, etc to exclude bad chunks from diff
+filter() {
+	cat
+}
+
+if [ ! -d $repo ]; then
+	git clone --bare $url -b $branch $repo
+fi
+
+cd $repo
+	git fetch origin +$branch:$branch +refs/tags/$tag:refs/tags/$tag
+	git log -p --reverse $tag..$branch | filter > ../$out.tmp
+cd ..
+
+if cmp -s $out{,.tmp}; then
+	echo >&2 "No new diffs..."
+	rm -f $out.tmp
+	exit 0
+fi
+mv -f $out{.tmp,}
+
+../md5 $package.spec
+../dropin $out
